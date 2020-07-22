59069294a05f99e6d2e43308fa351a5ba1e5785d
Nazar Broslavskyi <Nazar_Broslavskyi@epam.com>
2020-07-22 14:44:42 +0300

    refactor convert function

--- Module2/arrayToString.js
+++ Module2/arrayToString.js
@@ -6,14 +6,14 @@
   //output // '0,1,2,3,4\n' +
   //'10,11,12,13,14'
   
-  
+
 // 1. First solution
 
 let arrToString = arr => arr.join(`\n`)
 
 // 1. Second solutuion
 
-let arrToString2 = arr => arr.map(el => el.toString()).reduce((acum, el) => `${acum}\n${el}` , '')
+let arrToString2 = arr => arr.reduce((acum, el) => `${acum}${el}\n` , '')
 
 let input = [
   [0, 1, 2, 3, 4],
